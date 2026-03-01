cc14bf291643ae06ec29ef0442551fc966644e0b
zadockplant-jpg <zadockplant@gmail.com>
2026-03-01 15:34:30 -0500

    flutter autofix

--- comms-app/apps/mobile/lib/ui/screens/edit_contacts_screen.dart
+++ comms-app/apps/mobile/lib/ui/screens/edit_contacts_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import '../../core/app_state.dart';
 import '../../models/contact.dart';
